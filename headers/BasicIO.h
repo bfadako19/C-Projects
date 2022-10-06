96569743eab4b3972bf7be575777576db510a27a
Bryce Fadako <fadakob@chc.edu>
2022-10-06 10:47:27 -0400

    10/6/22

---
+++ headers/BasicIO.h
@@ -0,0 +1,10 @@
+// include pre-processor directives
+#define FIRST_NAME "Bryce"
+#define LAST_NAME "Fadako"
+#define WEIGHT 180.75
+#define AGE 21
+#include <stdio.h>
+
+// include function declarations
+void output(void);
+void input(void);

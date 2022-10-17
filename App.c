3cb449d8397e836ee3baed913260c08aeeb2fce0
Bryce Fadako <fadakob@chc.edu>
2022-10-16 21:02:37 -0400

    10/16/22

--- App.c
+++ App.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 #include "./headers/BasicIO.h"
 #include "./headers/Decisions.h"
+#include "./headers/Loops.h"
 
 int main(void){
     // puts ("Hello World");
+    //testing BasicIO functionality
     // output();
     // input();
+    //testing Decisions functionality
     //decisions1();
     //decisions2();
     //decisions3();
     //decisions4();
     //decisions5();
-    decisions6();
+    //decisions6();
+    //testing Loops functionality
+    loops1();
+    loops2();
+    loops3();
+    loops4();
+    loops5();
+
 }
